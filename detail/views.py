4390564166311364524f12b8dbc122de79ac8359
HateMyselfForLovingU <1227277547@qq.com>
2020-05-19 15:15:25 +0800

    test

--- detail/views.py
+++ detail/views.py
@@ -6,4 +6,20 @@ from utils.funcs import *
 
 @login_required
 def detail(request, class_id):
-    return
+    cl = Class.objects.get(id=class_id)
+    class_name = cl.class_name
+    students = StudentMembership.objects.filter(class_id=cl).all()
+    tempList = []
+    for student in students:
+        tempList.append({
+            "name": student.id.usrname,
+            "id": student.student_id,
+            "url": student.ref_photo_url
+        })
+    length = len(tempList)
+    return render(request, "detail/detail.html", {
+        "class_id": class_id,
+        "student_lists": tempList,
+        "class_name": class_name,
+        "len": length
+    })
